7902d1f6951e7ce6503de8f2e8f0c27aa7b9fc4c
Harsh <kumarharsh93982@gmail.com>
2026-05-23 06:42:56 +0530

    env configuration for AWS S3.

--- server/src/config/env.config.ts
+++ server/src/config/env.config.ts
@@ -15,6 +15,11 @@ const envConfig = {
   SMTP_PASSWORD: process.env.SMTP_PASSWORD,
   SMTP_SERVICE: process.env.SMTP_SERVICE,
 
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  AWS_REGION: process.env.AWS_REGION,
+  AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
+
 };
 
 export default envConfig;
