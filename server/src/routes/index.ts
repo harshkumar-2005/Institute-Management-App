da94287e9802ac72343dd1685603149f827e65f7
Harsh <kumarharsh93982@gmail.com>
2026-05-23 06:44:11 +0530

    students routes are introduced.

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "./auth.routes.js";
 import adminRoutes from "./admin.routes.js";
 import teacherRoutes from "./teacher.routes.js";
+import studentRoutes from "./student.routes.js";
 
 const router = express.Router();
 
@@ -10,5 +11,7 @@ router.use("/auth", authRoutes);
 router.use("/admin", adminRoutes);
 
 router.use("/teacher", teacherRoutes);
+
+router.use("/student", studentRoutes);
 
 export default router;
