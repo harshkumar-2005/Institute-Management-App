67f2d4d16ff26f53ff19b8daac684e910becd6a5
Harsh <kumarharsh93982@gmail.com>
2026-06-14 23:37:51 +0530

    Delete a paticular object of the s3 object via path.

--- server/src/services/aws/s3-Services/aws.s3.deleteObjects.ts
+++ server/src/services/aws/s3-Services/aws.s3.deleteObjects.ts
@@ -1,3 +1,37 @@
-const deleteFileFromS3 = async() => {
-    
-}
+import Client from "../../../lib/S3.client.js";
+import { DeleteObjectCommand } from "@aws-sdk/client-s3";
+import envConfig from "../../../config/env.config.js";
+
+const deleteObject = async (key: string) => {
+  const params = {
+    Bucket: envConfig.AWS_BUCKET_NAME || "IMS-454034543634234323-ap-south-1-an",
+    Key: key || "lol4.jpg",
+  };
+
+  try {
+    const data = await Client.send(new DeleteObjectCommand(params));
+    return { success: "true", message: "Deleted Successfully", data: data };
+  } catch (err: any) {
+    return { success: "false", message: err.message };
+  }
+};
+
+export default deleteObject;
+
+// example:-
+// await deleteObject("lol1.jpg");
+// output:-
+// {
+//   success: 'true',
+//   message: 'Deleted Successfully',
+//   data: {
+//     '$metadata': {
+//       httpStatusCode: 204,
+//       requestId: 'D74PYWXC8G',
+//       extendedRequestId: 'Y7JTxwxJkCCcsYvPMxZwneOA',
+//       cfId: undefined,
+//       attempts: 1,
+//       totalRetryDelay: 0
+//     }
+//   }
+// }
